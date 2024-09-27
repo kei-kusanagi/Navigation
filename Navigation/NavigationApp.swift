d27b3e05eb5ccee7b600c2b1907788bdca097c40
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-27 12:14:05 -0600

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Juan Carlos Robledo Morales on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
