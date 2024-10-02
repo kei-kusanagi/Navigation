f05f79fe5991097c6dc99386ae535cd9307027ef
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-02 12:50:42 -0600

    Personalizar la apariencia de la barra de navegación

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,62 +7,21 @@
 
 import SwiftUI
 
-@Observable
-class PathStore {
-    var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-
-    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
-
-    init() {
-        if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
-                path = NavigationPath(decoded)
-                return
-            }
-        }
-
-        // ¿Aún aquí? Empezar con una ruta vacía.
-        path = NavigationPath()
-    }
-
-    func save() {
-        guard let representation = path.codable else { return }
-
-        do {
-            let data = try JSONEncoder().encode(representation)
-            try data.write(to: savePath)
-        } catch {
-            print("Falló al guardar los datos de navegación")
-        }
-    }
-}
-
-struct DetailView: View {
-    var number: Int
-    
-    var body: some View{
-        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
-    }
-}
-
 struct ContentView: View {
-    @State private var pathStore = PathStore()
-
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i)
-                }
+        NavigationStack{
+            List(0..<100) { i in
+                Text("Fila \(i)")
+            }
+            .navigationTitle("El titulo va aqui")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.indigo)
+            .toolbarColorScheme(.dark)
+            .toolbar(.hidden)
         }
+        
     }
 }
-
 
 #Preview {
     ContentView()
